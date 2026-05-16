46fec867aa2f02868c7cedaf966880e44c1c48e6
Tanisha <jannatulnurtanisha63@gmail.com>
2026-05-16 12:43:43 +0600

    fixed my loader

--- src/components/shared/Loader.jsx
+++ src/components/shared/Loader.jsx
@@ -18,9 +18,10 @@ export default function Loader() {
             <div className="flex flex-col items-center gap-5">
                 {/* Spinner */}
                 <div className="relative">
-                    <div className="w-20 h-20 border-4 border-purple-200 rounded-full"></div>
+                    <div className="w-20 h-20 border-4 border-purple-200 border-t-purple-600 animate-spin  rounded-full"></div>
 
-                    <div className="absolute top-0 left-0 w-20 h-20 border-4 border-t-purple-600 border-b-transparent border-l-transparent rounded-full animate-spin "></div>
+                    <div className="absolute top-0 left-0 w-20 h-20 border-4 border-t-purple-600 border-r-purple-600
+                    border-b-transparent border-l-transparent rounded-full animate-spin "></div>
                     {/* Text */}
                     <div className="text-center">
                         <h2 className="text-2xl font-bold bg-linear-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent inline-block">
